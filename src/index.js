e3574cb9826a2c08e44f232509c4495bee97f4e9
Arekkasu <alexanderlozada230@gmail.com>
2023-09-18 10:59:32 -0500

    s

--- src/index.js
+++ src/index.js
@@ -7,5 +7,11 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'src/index.html'));
+
+});
+
 
 app.listen(7000, () => console.log('Servidor corriendo en el puerto 3000'));
