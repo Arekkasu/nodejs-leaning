149b8c7e00f27783a6ea44a897b38f0d688ef9e8
Arekkasu <alexanderlozada230@gmail.com>
2023-09-18 11:30:06 -0500

    ddd

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.get('/', (req, res) => {
 
-    res.sendFile(path.join(__dirname, 'src/index.html'));
+    res.sendFile(path.join(__dirname, 'src/public/index.html'));
 
 });
 
